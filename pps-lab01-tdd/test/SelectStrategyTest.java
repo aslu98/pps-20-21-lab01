3a48154f9cb3477f5d65fae7996d6a5a54e1eb18
Asia <asia.lucchi@studio.unibo.it>
2021-03-03 10:33:28 +0100

    SelectStrategyTest refactoring: using magic numbers for readability

--- pps-lab01-tdd/test/SelectStrategyTest.java
+++ pps-lab01-tdd/test/SelectStrategyTest.java
@@ -29,67 +29,51 @@ public class SelectStrategyTest {
 
     @Test
     void testEvenStrategy(){
-        final int oddNumSecond = 1;
-        final int oddNumFourth = 3;
-        final int evenNumFirst = 2;
-        final int evenNumThird = 4;
-        list.add(evenNumFirst);
-        list.add(oddNumSecond);
-        list.add(evenNumThird);
-        list.add(oddNumFourth);
+        list.add(2);
+        list.add(1);
+        list.add(4);
+        list.add(3);
         SelectStrategy strategy = strategyFactory.getStrategy(Strategies.EVEN, Optional.empty());
-        assertEquals(list.next(), Optional.of(evenNumFirst));
-        assertEquals(list.next(strategy), Optional.of(evenNumThird));
-        assertEquals(list.next(strategy), Optional.of(evenNumFirst));
-        assertEquals(list.next(), Optional.of(oddNumSecond));
+        assertEquals(list.next(), Optional.of(2));
+        assertEquals(list.next(strategy), Optional.of(4));
+        assertEquals(list.next(strategy), Optional.of(2));
+        assertEquals(list.next(), Optional.of(1));
     }
 
     @Test
     void testMultipleOfStrategy(){
-        final int multipleNumFirst = 3;
-        final int multipleNumFourth = 6;
-        final int notMultipleNumSecond = 1;
-        final int notMultipleNumThird = 2;
-        final int notMultipleNumFifth = 4;
-        list.add(multipleNumFirst);
-        list.add(notMultipleNumSecond);
-        list.add(notMultipleNumThird);
-        list.add(multipleNumFourth);
-        list.add(notMultipleNumFifth);
+        list.add(3);
+        list.add(1);
+        list.add(2);
+        list.add(6);
+        list.add(4);
         SelectStrategy strategy = strategyFactory.getStrategy(Strategies.MULTIPLE_OF, Optional.of(3));
-        assertEquals(list.next(), Optional.of(multipleNumFirst));
-        assertEquals(list.next(strategy), Optional.of(multipleNumFourth));
-        assertEquals(list.next(strategy), Optional.of(multipleNumFirst));
-        assertEquals(list.next(strategy), Optional.of(multipleNumFourth));
-        assertEquals(list.next(), Optional.of(notMultipleNumFifth));
+        assertEquals(list.next(), Optional.of(3));
+        assertEquals(list.next(strategy), Optional.of(6));
+        assertEquals(list.next(strategy), Optional.of(3));
+        assertEquals(list.next(strategy), Optional.of(6));
+        assertEquals(list.next(), Optional.of(4));
     }
 
     @Test
     void testEqualsStrategy(){
-        final int equalsNum = 6;
-        final int equalsNumFirst = equalsNum;
-        final int equalsNumFourth = equalsNum;
-        final int notEqualsNumSecond = 1;
-        final int notEqualsNumThird = 2;
-        final int notEqualsNumFifth = 4;
-        list.add(equalsNumFirst);
-        list.add(notEqualsNumSecond);
-        list.add(notEqualsNumThird);
-        list.add(equalsNumFourth);
-        list.add(notEqualsNumFifth);
-        SelectStrategy strategy = strategyFactory.getStrategy(Strategies.EQUALS, Optional.of(equalsNum));
-        assertEquals(list.next(), Optional.of(equalsNumFirst));
-        assertEquals(list.next(strategy), Optional.of(equalsNumFourth));
-        assertEquals(list.next(strategy), Optional.of(equalsNumFirst));
-        assertEquals(list.next(), Optional.of(notEqualsNumSecond));
+        list.add(6);
+        list.add(1);
+        list.add(2);
+        list.add(6);
+        list.add(4);
+        SelectStrategy strategy = strategyFactory.getStrategy(Strategies.EQUALS, Optional.of(6));
+        assertEquals(list.next(), Optional.of(6));
+        assertEquals(list.next(strategy), Optional.of(6));
+        assertEquals(list.next(strategy), Optional.of(6));
+        assertEquals(list.next(), Optional.of(1));
     }
 
     @Test
     void testEmptyListWithStrategy(){
-        final int notEqualsNum = 1;
-        final int equalsNum = 6;
-        list.add(notEqualsNum);
-        SelectStrategy strategy = strategyFactory.getStrategy(Strategies.EQUALS, Optional.of(equalsNum));
+        list.add(1);
+        list.add(2);
+        SelectStrategy strategy = strategyFactory.getStrategy(Strategies.EQUALS, Optional.of(6));
         assertEquals(list.next(strategy), Optional.empty());
     }
 
